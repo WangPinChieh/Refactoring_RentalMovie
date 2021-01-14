949cdd944b6c6e3e4a1fa6c93158f6744562c86f
WangPinChieh <pp8101313@hotmail.com>
2021-01-14 09:04:31 +0800

    add tests

--- RentalMovieTests.cs
+++ RentalMovieTests.cs
@@ -36,6 +36,16 @@ namespace Refactoring_RentalMovie
                 "Rental Record for Jay\n\tNewRelease 1\t30\nAmount owed is 30\nYou earned 2 frequent renter points");
         }
 
+        [Test]
+        public void customer_rent_all_kind_of_movies()
+        {
+            GivenRentals(new Rental(new Movie("NewRelease 1", Movie.NewRelease), 10));
+            GivenRentals(new Rental(new Movie("Children 1", Movie.Children), 4));
+            GivenRentals(new Rental(new Movie("Regular 1", Movie.Regular), 17));
+            StatementShouldBe(
+                "Rental Record for Jay\n\tNewRelease 1\t30\n\tChildren 1\t3\n\tRegular 1\t24.5\nAmount owed is 57.5\nYou earned 4 frequent renter points");
+        }
+
         private void GivenRentals(Rental rental)
         {
             _customer.AddRental(rental);
